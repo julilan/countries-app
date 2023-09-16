9a7d9a4d156676839a0c7b84e277275c3ce613ed
julilan <juli.molnar@outlook.com>
2023-09-16 13:14:42 +0300

    render countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,73 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-
-import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
-import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { initializeCountries } from '../features/countries/countriesSlice';
+import CountryCard from './CountryCard';
 
 const Countries = () => {
-  const [search, setSearch] = useState('')
+  const dispatch = useDispatch();
+  const countriesList = useSelector((state) => state.countries.countries);
+  const loading = useSelector((state) => state.countries.isLoading);
 
-  console.log("Search: ", search)
+  console.log('CountriesList: ', countriesList);
 
-  // We will be replacing this with data from our API.
-  const country = {
-    name: {
-      common: 'Example Country'
-    }
-  }
+  const [search, setSearch] = useState('');
+
+  console.log('Search: ', search);
+
+  useEffect(() => {
+    dispatch(initializeCountries());
+  }, [dispatch]);
 
   return (
     <Container fluid>
       <Row>
-        <Col className="mt-5 d-flex justify-content-center">
+        <Col className='mt-5 d-flex justify-content-center'>
           <Form>
             <Form.Control
               style={{ width: '18rem' }}
-              type="search"
-              className="me-2 "
-              placeholder="Search for countries"
-              aria-label="Search"
+              type='search'
+              className='me-2 '
+              placeholder='Search for countries'
+              aria-label='Search'
               onChange={(e) => setSearch(e.target.value)}
             />
           </Form>
         </Col>
       </Row>
-      <Row xs={2} md={3} lg={4} className=" g-3">
-            <Col className="mt-5">
-              <LinkContainer
-                to={`/countries/${country.name.common}`}
-                state={{ country: country }}
-              >
-                <Card className="h-100">
-                  <Card.Body className="d-flex flex-column">
-                    <Card.Title>{'Single Country Common Name'}</Card.Title>
-                    <Card.Subtitle className="mb-5 text-muted">
-                      {'Single Country Official Name'}
-                    </Card.Subtitle>
-                    <ListGroup
-                      variant="flush"
-                      className="flex-grow-1 justify-content-end"
-                    >
-                      <ListGroup.Item>
-                        <i className="bi bi-translate me-2"></i>
-                      </ListGroup.Item>
-                      <ListGroup.Item>
-                        <i className="bi bi-cash-coin me-2"></i>
-                      </ListGroup.Item>
-
-                      <ListGroup.Item>
-                        <i className="bi bi-people me-2"></i>
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </Card.Body>
-                </Card>
-              </LinkContainer>
-            </Col>
+      <Row xs={2} md={3} lg={4} className=' g-3'>
+        {/* Search filter */}
+        {countriesList.map((country) => {
+          return <CountryCard country={country} key={country.name.common} />;
+        })}
       </Row>
     </Container>
   );
--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-
-import { Container } from 'react-bootstrap';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Col, Container } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const CountriesSingle = () => {
+  // Function hooks
+  const location = useLocation();
+  const navigate = useNavigate();
 
+  // State hooks
+  const [weather, setWeather] = useState('');
+  const [errors, setErrors] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  // Destructuring
+  const country = location.state.country;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${process.env.REACT_APP_OPENWEATHER_KEY}`
+      )
+      .catch((err) => {
+        setErrors(true);
+      })
+      .then((res) => {
+        setWeather(res.data);
+        setLoading(false);
+      });
+  }, [country.capital]);
+
+  console.log('Weather: ', weather);
 
   return (
     <Container>
-      <div>Single Country will be here</div>
+      <Col className='mt-5'>
+        <h2>{country.name.common}</h2>
+        <p>Temperature: {weather.main.temp}</p>
+        <p>{weather.weather[0].description}</p>
+      </Col>
     </Container>
   );
 };
