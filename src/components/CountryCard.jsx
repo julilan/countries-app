37a259f99119ac2d377a01f0c6d106ef3e82724d
julilan <juli.molnar@outlook.com>
2023-09-22 01:37:36 +0300

    add flag, languages, currencies and population

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -3,6 +3,16 @@ import { Card, Col, ListGroup } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const CountryCard = ({ country }) => {
+  const languages = [];
+  for (const language in country.languages) {
+    languages.push(country.languages[language]);
+  }
+  //console.log(country.flags.png);
+
+  // country.currencies
+  //   ? console.log(Object.values(country.currencies)[0].name)
+  //   : console.log('no currency found');
+
   return (
     <Col className='mt-5'>
       <LinkContainer
@@ -15,18 +25,22 @@ const CountryCard = ({ country }) => {
             <Card.Subtitle className='mb-5 text-muted'>
               {country.name.official}
             </Card.Subtitle>
+            <Card.Img src={country.flags.svg} alt='flag' />
             <ListGroup
               variant='flush'
               className='flex-grow-1 justify-content-end'
             >
               <ListGroup.Item>
-                <i className='bi bi-translate me-2'></i>
+                <i className='bi bi-translate me-2'></i> {languages.join(', ')}
               </ListGroup.Item>
               <ListGroup.Item>
-                <i className='bi bi-cash-coin me-2'></i>
+                <i className='bi bi-cash-coin me-2'></i>{' '}
+                {country.currencies
+                  ? Object.values(country.currencies)[0].name
+                  : 'no currency'}
               </ListGroup.Item>
               <ListGroup.Item>
-                <i className='bi bi-people me-2'></i>
+                <i className='bi bi-people me-2'></i> {country.population}
               </ListGroup.Item>
             </ListGroup>
           </Card.Body>
