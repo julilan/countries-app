ed26638bf505a7b84e88f6823508029f1637c9b1
julilan <juli.molnar@outlook.com>
2023-09-22 14:42:33 +0300

    Add new routes for auth

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { logout } from '../auth/firebase';
 
 const Layout = () => {
   return (
     <Container fluid>
       <Row>
-        <Navbar bg="light" variant="light">
-          <Container className="justify-content-end">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar bg='light' variant='light'>
+          <Container className='justify-content-end'>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
               <Nav>
-                <LinkContainer to="/">
+                <LinkContainer to='/'>
                   <Nav.Link>Home</Nav.Link>
                 </LinkContainer>
-                <LinkContainer to="/countries">
+                <LinkContainer to='/countries'>
                   <Nav.Link>Countries</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/register'>
+                  <Nav.Link>Register</Nav.Link>
                 </LinkContainer>
               </Nav>
             </Navbar.Collapse>
+            <Button onClick={logout}>Logout</Button>
           </Container>
         </Navbar>
       </Row>
