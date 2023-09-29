90c4204e8e3fa0df99a6e817f66c64d9942d5fb1
julilan <juli.molnar@outlook.com>
2023-09-29 16:50:53 +0300

    loading check and rendering

--- src/auth/ProtectedRoute.js
+++ src/auth/ProtectedRoute.js
@@ -1,6 +1,13 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, Outlet } from 'react-router-dom';
+import { auth } from './firebase';
 
-const ProtectedRoute = ({ user, children }) => {
+const ProtectedRoute = () => {
+
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   if (!user) {
     return <Navigate to='/login' />;
   }
