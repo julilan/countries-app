83eed17fd0ece572ae86a8b9cc94b1a2e596b71d
julilan <juli.molnar@outlook.com>
2023-10-13 23:49:47 +0300

    fix and style GitHub Button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,27 +24,16 @@ const Home = () => {
             A React application made in Business College Helsinki lessons.
           </p>
         </div>
-        <Button>
-          <a
-            target='_blank'
-            rel='noreferrer'
-            href='https://github.com/julilan/countries-app'
-            className='text-decoration-none text-light'
-          >
-            <i className='bi bi-github'></i> GitHub
-          </a>
+        <Button
+          variant='outline-light'
+          size='lg'
+          href='https://github.com/julilan/countries-app'
+          target='_blank'
+          ref={(el) => el && el.setAttribute('rel', 'noopener noreferrer')}
+        >
+          <i className='bi bi-github'></i> GitHub
         </Button>
       </div>
-      {/* <div>
-        <h2>Welcome to Countries App!</h2>
-        <p>A React application made in Business College Helsinki lessons.</p>
-        <p>
-          This app uses{' '}
-          <a href='https://restcountries.com/'>https://restcountries.com/ </a>{' '}
-          and{' '}
-          <a href='https://openweathermap.org/'>https://openweathermap.org/</a>
-        </p>
-      </div> */}
     </div>
   );
 };
