c05bbfe937b29ecfebebe9ecf4a25a071c420173
julilan <juli.molnar@outlook.com>
2023-12-27 22:37:25 +0200

    update styling for Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { LinkContainer } from 'react-router-bootstrap';
-import { auth } from '../auth/firebase';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { LinkContainer } from "react-router-bootstrap";
+import { auth } from "../auth/firebase";
 
 const Home = () => {
   const [user, loading] = useAuthState(auth);
@@ -13,11 +13,11 @@ const Home = () => {
       style={{
         backgroundImage:
           "url('https://images.unsplash.com/photo-1589403992174-da57ba171563?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')",
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: '#444',
-        backgroundBlendMode: 'overlay',
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "#444",
+        backgroundBlendMode: "overlay",
       }}
     >
       <div className='align-self-center text-center text-light col-md-8 offset-md-2'>
@@ -29,28 +29,31 @@ const Home = () => {
             A React application made in Business College Helsinki lessons.
           </p>
         </div>
-        {!user && (
-          <>
-            <p>Start browsing countries by logging in!</p>
-            <LinkContainer to='/login'>
-              <Button variant='primary' hidden={loading} className='mb-3'>
-                Login <i className='bi bi-arrow-right-short'></i>
-              </Button>
-            </LinkContainer>
-          </>
-        )}
-        <p>
-          <strong>Check out the source code:</strong>
-        </p>
-        <Button
-          variant='outline-light'
-          size='lg'
-          href='https://github.com/julilan/countries-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <i className='bi bi-github'></i> GitHub
-        </Button>
+        <div className='d-flex flex-column flex-sm-row justify-content-center gap-2 gap-sm-5'>
+          {!user && (
+            <>
+              <div className='mb-2'>
+                <p>Start browsing countries by logging in!</p>
+                <LinkContainer to='/login'>
+                  <Button variant='primary' hidden={loading}>
+                    Login <i className='bi bi-arrow-right-short'></i>
+                  </Button>
+                </LinkContainer>
+              </div>
+            </>
+          )}
+          <div>
+            <p>Check out the source code:</p>
+            <Button
+              variant='outline-light'
+              href='https://github.com/julilan/countries-app'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='bi bi-github'></i> GitHub
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
